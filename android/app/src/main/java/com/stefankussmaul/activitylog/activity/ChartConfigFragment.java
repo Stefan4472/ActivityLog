bb2391ab7fb0bb71dced0d85e5598b6f59d3dbee
stefan4472 <stefan.kussmaul@gmail.com>
2017-05-31 19:02:17 -0400

    ChartConfigFragment Logic And Setup

--- android/app/src/main/java/com/stefankussmaul/activitylog/activity/ChartConfigFragment.java
+++ android/app/src/main/java/com/stefankussmaul/activitylog/activity/ChartConfigFragment.java
@@ -3,16 +3,18 @@ package com.stefankussmaul.activitylog.activity;
 import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.stefankussmaul.activitylog.R;
-import com.stefankussmaul.activitylog.content.DateUtil;
-import com.stefankussmaul.activitylog.content.QueryBuilder;
+import com.stefankussmaul.activitylog.charts.ChartConfig;
 
-import java.util.Calendar;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Fragment that provides options for configuring the AnalyticsActivity chart
@@ -20,20 +22,30 @@ import java.util.Calendar;
 
 public class ChartConfigFragment extends Fragment {
 
+    private Spinner chartTypeSpinner;
+    private Spinner chartBySpinner;
+    private ChartConfig configuration;
+    private OnConfigChangeListener mListener;
+
+    public interface OnConfigChangeListener {
+        void onConfigChanged(ChartConfigFragment chartConfigFragment, ChartConfig config);
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        configuration = new ChartConfig(ChartConfig.ChartType.PIE, ChartConfig.ChartBy.NUM_SESSIONS);
     }
 
-//    @Override // ensures activity implements OnFilterUpdatedListener
-//    public void onAttach(Activity activity) {
-//        super.onAttach(activity);
-//        try {
-//            mListener = (LogFilterFragment.OnFilterUpdatedListener) activity;
-//        } catch (ClassCastException e) {
-//            throw new ClassCastException(activity.toString() + " must implement OnFilterUpdatedListener");
-//        }
-//    }
+    @Override // ensures activity implements OnFilterUpdatedListener
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnConfigChangeListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnConfigChangeListener");
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -45,5 +57,63 @@ public class ChartConfigFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
 
         View view = getView();
+
+        chartTypeSpinner = (Spinner) view.findViewById(R.id.chart_type_spinner);
+        List<String> type_choices = new LinkedList<>();
+        type_choices.add(getString(R.string.pie_chart));
+        type_choices.add(getString(R.string.line_chart));
+        // create adapter using the choices
+        ArrayAdapter<String> type_adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, type_choices);
+        chartTypeSpinner.setAdapter(type_adapter);
+
+        chartTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String type = (String) parent.getItemAtPosition(position);
+                if (type.equals(getString(R.string.pie_chart))) {
+                    configuration.setChartType(ChartConfig.ChartType.PIE);
+                } else if (type.equals(getString(R.string.line_chart))) {
+                    configuration.setChartType(ChartConfig.ChartType.LINE);
+                } else {
+                    throw new IllegalArgumentException("Unrecognized Type '" + type + "'. Invalid Spinner option.");
+                }
+                mListener.onConfigChanged(ChartConfigFragment.this, configuration);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        chartBySpinner = (Spinner) view.findViewById(R.id.chart_by_spinner);
+        List<String> by_choices = new LinkedList<>();
+        by_choices.add(getString(R.string.num_sessions));
+        by_choices.add(getString(R.string.time_spent));
+        // create adapter using the choices
+        ArrayAdapter<String> by_adapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_spinner_item, by_choices);
+        chartBySpinner.setAdapter(by_adapter);
+
+        chartBySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String type = (String) parent.getItemAtPosition(position);
+                if (type.equals(getString(R.string.time_spent))) {
+                    configuration.setChartBy(ChartConfig.ChartBy.TOTAL_DURATION);
+                } else if (type.equals(getString(R.string.num_sessions))) {
+                    configuration.setChartBy(ChartConfig.ChartBy.NUM_SESSIONS);
+                } else {
+                    throw new IllegalArgumentException("Unrecognized Type '" + type + "'. Invalid Spinner option.");
+                }
+                mListener.onConfigChanged(ChartConfigFragment.this, configuration);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
---
+++ android/app/src/main/java/com/stefankussmaul/activitylog/charts/ChartConfig.java
@@ -0,0 +1,39 @@
+package com.stefankussmaul.activitylog.charts;
+
+/**
+ * Defines a configuration for the AnalyticsActivity chart
+ */
+
+public class ChartConfig {
+
+    public enum ChartType {
+        PIE, LINE;
+    }
+    public enum ChartBy {
+        NUM_SESSIONS, TOTAL_DURATION;
+    }
+
+    private ChartType chartType;
+    private ChartBy chartBy;
+
+    public ChartConfig(ChartType chartType, ChartBy chartBy) {
+        this.chartType = chartType;
+        this.chartBy = chartBy;
+    }
+
+    public ChartType getChartType() {
+        return chartType;
+    }
+
+    public ChartBy getChartBy() {
+        return chartBy;
+    }
+
+    public void setChartType(ChartType chartType) {
+        this.chartType = chartType;
+    }
+
+    public void setChartBy(ChartBy chartBy) {
+        this.chartBy = chartBy;
+    }
+}
